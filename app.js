821d1231720a70e9ac4ee44a84434e523d9b2eee
Nitasha <nitasha464@gmail.com>
2023-06-02 10:17:49 +0530

    chore: initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+var getTranslationURL = serverURL + "?" + "text=" + txtInput.value;
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something went wrong! try again after some time");
+}
+
+function clickEventHandler() {
+    fetch(getTranslationURL)
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
